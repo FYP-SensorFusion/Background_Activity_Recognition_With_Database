72bedc81472f3a1f23d539d5f84f532e1623912d
Sathira Liyanapathirana <liyanapathiranasathira@gmail.com>
2024-04-27 02:40:59 +0530

    Login, Sign In, Logout pages added - Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
-import 'package:background_bctivity_recognition_with_database/screens/activities_screen.dart';
-import 'package:background_bctivity_recognition_with_database/screens/activity_report.dart';
+import 'package:background_bctivity_recognition_with_database/screens/signin_screen.dart';
 import 'package:background_bctivity_recognition_with_database/services/activity_recognition_function.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyCxxWO7_2n24I0okFrzI979GrcPX_Dco1I",
+    authDomain: "healthspike-86c82.firebaseapp.com",
+    databaseURL: "https://healthspike-86c82-default-rtdb.firebaseio.com",
+    projectId: "healthspike-86c82",
+    storageBucket: "healthspike-86c82.appspot.com",
+    messagingSenderId: "701517480193",
+    appId: "1:701517480193:web:1acad2d558c13a08cedb00",
+    measurementId: "G-106SNJYJE8",
+  ));
   if (await _arePermissionsGranted()) {
-  FlutterBackgroundService.initialize(onStart);
+    FlutterBackgroundService.initialize(onStart);
   }
   runApp(const MyApp());
 }
@@ -66,79 +77,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Background Activity Recognition'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    requestPermissions();
-  }
-
-  void requestPermissions() async {
-    // You can request multiple permissions at once.
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.location,
-      Permission.activityRecognition,
-    ].request();
-
-    print(statuses[Permission.location]);
-    print(statuses[Permission.activityRecognition]);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Activity Recognition'),
-          centerTitle: true,
-        ),
-        body: Container(),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              heroTag: "activity_screan",
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ActivitiesScreen()),
-                );
-              },
-              child: const Icon(Icons.view_list),
-            ),
-            const SizedBox(height: 16),
-            FloatingActionButton(
-              heroTag: "activity_report",
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ActivityReportScreen()),
-                );
-                // Add logic to handle the activity report button
-                // Redirect to the report screen or perform any other action
-              },
-              child: const Icon(Icons.analytics),
-            ),
-          ],
-        ),
-      ),
+      home: SignInScreen(),
     );
   }
 }
